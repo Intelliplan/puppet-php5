9e9a304c2b999ea28a4bb1572bef21471af30250
Kamil Hakim <kamil.hakim.swe@gmail.com>
2015-06-25 10:32:02 +0200

    removed augeas dependency

--- manifests/ini.pp
+++ manifests/ini.pp
@@ -4,24 +4,6 @@ define php5::ini (
 ){
   require php5
 
-
-  @package {'augeas':
-    ensure => installed
-  }
-  realize Package[['augeas']]
-
-
-  #case $::osfamily {
-  #  /(?i:redhat)/: { 
-  #    $augeas_package_name = 'augeas'
-  #    $augtool_path        = ['/usr/local/bin', '/bin', '/usr/bin']
-  #  }
-  #  default: { fail("Unsupported platform: ${::osfamily}") }
-  #}
-  #package { $augeas_package_name:
-  #  ensure => 'present'
-  #}
-
   case $ensure {
     undef: { fail('Ensure must be specified') }
     /(?i:absent|clear)/: {
@@ -31,12 +13,6 @@ define php5::ini (
         notify  => Service['apache2-service'],
         require => Package['augeas'],
       }
-      #exec { "php::ini::clear::$title":
-      #  command => "augtool clear '/files/etc/php.ini/$title'",
-      #  path    => $augtool_path,
-      #  notify  => Service['apache2-service'],
-      #  require => Package[$augeas_package_name],
-      #}
     }
     default : {
       augeas { "php::ini::set:${title}":
@@ -45,12 +21,6 @@ define php5::ini (
         notify  => Service['apache2-service'],
         require => Package['augeas'],
       }
-      #exec { "php::ini::set::$title":
-      #  command => "augtool set '/files/etc/php.ini/$title' '$ensure'",
-      #  path    => $augtool_path,
-      #  notify  => Service['apache2-service'],
-      #  require => Package[$augeas_package_name],
-      #}
     }
   }
 }
